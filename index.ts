0ab745854bdf1d67201235a9e9674dc6793cc150
K G A N <86733485+kganallinone@users.noreply.github.com>
2025-11-04 04:52:47 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ const startServer = async () => {
     const port = process.env.PORT || config.PORT;
     const io = new SocketIOServer(server, {
       cors: {
-        origin: "http://localhost:5173",
+        origin: "https://ojt-ms-app.web.app/",
         credentials: true,
       },
     });
